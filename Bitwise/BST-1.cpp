c34320ae12b9320cb98eaaf2e4adabdb5bf45090
Vishal Gorai <vishalgorai09@gmail.com>
2021-02-27 22:21:45 +0530

    Create BST-1.cpp
    construction a BST with O(n^2) complexity

---
+++ Bitwise/BST-1.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node {
+    public:
+    int data;
+    node *left, *right;
+};
+
+node *newNode(int data) {
+    node *temp = new node();
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    
+    return temp;
+}
+
+node *constructTree(int pre[], int *preIndex, int low, int high, int size) {
+    
+    if(*preIndex >= size || low > high) return NULL;
+    
+    node *root = newNode(pre[*preIndex]);
+    *preIndex += 1;
+    
+    if(low == high ) return root;
+    
+    int i;
+    for( i=low; i<=high; ++i) 
+        if(pre[i]>root->data) break;
+        
+    root->left = constructTree(pre, preIndex, *preIndex, i-1, size);
+    
+    root->right = constructTree(pre, preIndex, i, high, size);
+    
+    return root;
+}
+
+
+node* Tree(int pre[], int size) {
+    int preIndex = 0;
+    return constructTree(pre, &preIndex, 0, size-1, size);
+} 
+
+
+void printInorder(node *root){
+    if(!root) return;
+    printInorder(root->left);
+    cout<<root->data<< " ";
+    printInorder(root->right);
+}
+
+int main()
+{
+    int pre[] = {5,8,9,10, 11,12,20};
+    int size = sizeof(pre)/sizeof(pre[0]);
+    node *root = Tree(pre, size);
+    printInorder(root);
+    
+    return 0;
+}
