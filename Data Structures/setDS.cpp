37447620142d20ffe774251c89701d4d57498bfc
Vishal Gorai <vishalgorai09@gmail.com>
2021-03-10 14:01:13 +0530

    Create setDS.cpp

---
+++ Data Structures/setDS.cpp
@@ -0,0 +1,67 @@
+// Properties of set data structure in c++
+// -> unique values
+// -> duplicate values will not be inserted in the firse place
+// -> values are inserted in increasing order
+// -> set works on Balanced Binary Tree internally [T:O(log n)]
+
+// -> unordered_set works on Hashing [T:O(1)]
+// -> insertion is always randomised, and not in increasing order
+// -> unique elements are retained
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    set<int> s;
+    
+    s.insert(10);
+    s.insert(20);
+    s.insert(30);
+    s.insert(30); // this 30 will not be added
+    s.insert(50);
+    s.insert(5);
+    
+    set<int>::iterator i;
+    
+    // this prints the set in increasing order
+    cout<<"Set 1:\n";
+    for(i=s.begin(); i!=s.end(); i++) {
+        cout<<*i<<" ";
+    }
+    
+    cout<<endl;
+    // creating a new set with same values as of s1;
+    set<int> s2(s.begin(), s.end());
+    
+    cout<<"Set 2:\n";
+    for(i=s2.begin(); i!=s2.end(); i++) {
+        cout<<*i<<" ";
+    }    
+    
+    cout<<endl;
+    // cout<<*(s2.find(30))<<endl;
+    
+    unordered_set <int> us;
+    
+    us.insert(100);
+    us.insert(200);
+    us.insert(500);
+    us.insert(300);
+    us.insert(200);
+    us.insert(900);
+    us.insert(600);
+    
+    unordered_set<int>::iterator it;
+    
+    cout<<"Unordered Set 1:\n";
+    for(it = us.begin(); it!=us.end(); it++) {
+        cout<<*it<<" ";
+    }
+    
+    cout<<endl;
+    
+    
+    return 0;
+}
