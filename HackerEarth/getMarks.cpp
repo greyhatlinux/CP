a3b5b197ffb166209654443b5d3e28374879b3b5
Vishal Gorai <vishalgorai09@gmail.com>
2021-03-10 21:43:18 +0530

    Update getMarks.cpp

--- HackerEarth/getMarks.cpp
+++ HackerEarth/getMarks.cpp
@@ -15,7 +15,8 @@ int main()
 
 	cout<<endl;
 	for(int i=0;i<n;i++) {
-
+		
+		// (i==0) and (i==n-1) are for handling edge cases 
 		if(i==0) {
 			if(arr[i+1]<arr[i]+k) ctr++;
 		}
@@ -24,6 +25,7 @@ int main()
 			if(arr[i-1]>arr[i]-k) ctr++;
 		}
 		
+		// this is the main logic for the problem
 		else {
 			if((arr[i-1]>=arr[i]-k) || (arr[i+1]<=arr[i]+k)) ctr++;
 		}
